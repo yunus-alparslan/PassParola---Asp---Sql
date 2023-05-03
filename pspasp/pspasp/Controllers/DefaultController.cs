7a6377901c59ada047ad8019a042da37912cefa9
Yunus ALPARSLAN <99866054+yunus-alparslan@users.noreply.github.com>
2023-05-03 23:08:08 +0300

    PssASP

---
+++ pspasp/pspasp/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pspasp.Models.Enty;
+
+namespace pspasp.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        PspPrlEntities psp = new PspPrlEntities();
+
+        public ActionResult Index()
+        {
+            var psd = psp.Table_Soru.ToList();
+          //  psd.
+
+
+            return View(psd);
+        }
+    }
+}
---
+++ pspasp/pspasp/Controllers/SORUEKLEController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pspasp.Models.Enty;
+
+namespace pspasp.Controllers
+{
+    public class SORUEKLEController : Controller
+    {
+
+        PspPrlEntities soruis = new PspPrlEntities();
+        // GET: SORUEKLE
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+          [HttpGet]
+        public ActionResult soruekle()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult soruekle(Table_Soru sr){
+
+            soruis.Table_Soru.Add(sr);
+
+            soruis.SaveChanges();
+
+
+            return View(sr);
+        }
+    }
+}
